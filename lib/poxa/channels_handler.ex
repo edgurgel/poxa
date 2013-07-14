1d33c6452db4d322fd01feaacc6b44add088a4a6
Eduardo Gurgel <edgurgel@gmail.com>
2013-07-14 20:54:49 -0300

    Verify info attributes on /channels/:channel

--- lib/poxa/channels_handler.ex
+++ lib/poxa/channels_handler.ex
@@ -18,6 +18,27 @@ defmodule Poxa.ChannelsHandler do
     {["GET"], req, state}
   end
 
+  @valid_attributes ["user_count", "subscription_count"]
+
+  def malformed_request(req, _state) do
+    {info, req} = :cowboy_req.qs_val("info", req, "")
+    attributes = String.split(info, ",")
+    {channel, req} = :cowboy_req.binding(:channel_name, req, nil)
+    if channel do
+      {malformed_request_one_channel?(attributes, channel), req, channel}
+    else
+      {false, req, nil}
+    end
+  end
+
+  defp malformed_request_one_channel?(attributes, channel) do
+    if Enum.all?(attributes, fn s -> Enum.member?(@valid_attributes, s) end) do
+      !PresenceSubscription.presence_channel?(channel) and Enum.member?(attributes, "user_count")
+    else
+      true
+    end
+  end
+
   def is_authorized(req, state) do
     AuthorizationHelper.is_authorized(req, state)
   end
@@ -26,12 +47,11 @@ defmodule Poxa.ChannelsHandler do
     {[{{"application", "json", []}, :get_json}], req, state}
   end
 
-  def get_json(req, state) do
-    {channel, req} = :cowboy_req.binding(:channel_name, req, nil)
+  def get_json(req, channel) do
     if channel do
-      show(channel, req, state)
+      show(channel, req, nil)
     else
-      index(req, state)
+      index(req, nil)
     end
   end
 
@@ -42,14 +62,14 @@ defmodule Poxa.ChannelsHandler do
 
   defp index(req, state) do
     channels =
-    Subscription.all_channels
-      |> Enum.filter_map(
-        fn channel ->
-          PresenceSubscription.presence_channel? channel
-        end,
-        fn channel ->
-          {channel, [user_count: PresenceSubscription.user_count(channel)]}
-        end)
+      Subscription.all_channels
+        |> Enum.filter_map(
+          fn channel ->
+            PresenceSubscription.presence_channel? channel
+          end,
+          fn channel ->
+            {channel, [user_count: PresenceSubscription.user_count(channel)]}
+          end)
     {JSEX.encode!(channels: channels), req, state}
   end
 
--- test/channels_handler_test.exs
+++ test/channels_handler_test.exs
@@ -21,21 +21,82 @@ defmodule Poxa.ChannelsHandlerTest do
     unload :cowboy_req
   end
 
+  test "malformed_request on multiple channels" do
+    expect(:cowboy_req, :qs_val, 3, {"subscription_count", :req})
+    expect(:cowboy_req, :binding, 3, {nil, :req})
+
+    assert malformed_request(:req, :state) == {false, :req, nil}
+
+    assert validate :cowboy_req
+  end
+
+  test "malformed_request on single channel with invalid attributes" do
+    expect(:cowboy_req, :qs_val, 3, {"subscription_count,message_count", :req})
+    expect(:cowboy_req, :binding, 3, {"channel", :req})
+    expect(PresenceSubscription, :presence_channel?, 1, false)
+
+    assert malformed_request(:req, :state) == {true, :req, "channel"}
+
+    assert validate PresenceSubscription
+    assert validate :cowboy_req
+  end
+
+  test "malformed_request on single channel with invalid attribute" do
+    expect(:cowboy_req, :qs_val, 3, {"message_count", :req})
+    expect(:cowboy_req, :binding, 3, {"channel", :req})
+    expect(PresenceSubscription, :presence_channel?, 1, false)
+
+    assert malformed_request(:req, :state) == {true, :req, "channel"}
+
+    assert validate PresenceSubscription
+    assert validate :cowboy_req
+  end
+
+  test "malformed_request on single channel with valid attributes" do
+    expect(:cowboy_req, :qs_val, 3, {"subscription_count", :req})
+    expect(:cowboy_req, :binding, 3, {"channel", :req})
+    expect(PresenceSubscription, :presence_channel?, 1, false)
+
+    assert malformed_request(:req, :state) == {false, :req, "channel"}
+
+    assert validate PresenceSubscription
+    assert validate :cowboy_req
+  end
+
+  test "malformed_request on single non presence-channel with invalid attributes" do
+    expect(:cowboy_req, :qs_val, 3, {"user_count", :req})
+    expect(:cowboy_req, :binding, 3, {"channel", :req})
+    expect(PresenceSubscription, :presence_channel?, 1, false)
+
+    assert malformed_request(:req, :state) == {true, :req, "channel"}
+
+    assert validate PresenceSubscription
+    assert validate :cowboy_req
+  end
+
+  test "malformed_request on single presence-channel with invalid attributes" do
+    expect(:cowboy_req, :qs_val, 3, {"user_count,subscription_count", :req})
+    expect(:cowboy_req, :binding, 3, {"presence-channel", :req})
+    expect(PresenceSubscription, :presence_channel?, 1, true)
+
+    assert malformed_request(:req, :state) == {false, :req, "presence-channel"}
+
+    assert validate PresenceSubscription
+    assert validate :cowboy_req
+  end
+
   test "get_json on a specific channel" do
-    expect(:cowboy_req, :binding, 3, {:channel, :req})
     expect(Subscription, :subscription_count, 1, 5)
     expected = [occupied: true, subscription_count: 5]
     expect(JSEX, :encode!, [{[expected], :encoded_json}])
 
-    assert get_json(:req, :state) == {:encoded_json, :req, :state}
+    assert get_json(:req, :state) == {:encoded_json, :req, nil}
 
     assert validate JSEX
     assert validate Subscription
-    assert validate :cowboy_req
   end
 
   test "get_json on every single channel" do
-    expect(:cowboy_req, :binding, 3, {nil, :req})
     expect(Subscription, :all_channels, 0, ["presence-channel", "private-channel"])
     expect(PresenceSubscription, :presence_channel?, [{["presence-channel"], true},
                                                       {["private-channel"], false}])
@@ -43,12 +104,11 @@ defmodule Poxa.ChannelsHandlerTest do
     expected = [channels: [{"presence-channel", user_count: 3}]]
     expect(JSEX, :encode!, [{[expected], :encoded_json}])
 
-    assert get_json(:req, :state) == {:encoded_json, :req, :state}
+    assert get_json(:req, nil) == {:encoded_json, :req, nil}
 
     assert validate JSEX
     assert validate Subscription
     assert validate PresenceSubscription
-    assert validate :cowboy_req
   end
 
 end
