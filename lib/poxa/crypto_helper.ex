86c4dac4c6648830ca7e542abcf3215ac348d597
Eduardo Gurgel <edgurgel@gmail.com>
2014-01-01 22:27:02 -0300

    Add tests and doc to CryptoHelper

--- lib/poxa/crypto_helper.ex
+++ lib/poxa/crypto_helper.ex
@@ -1,4 +1,7 @@
 defmodule Poxa.CryptoHelper do
+  @doc """
+  Compute a SHA-256 MAC message authentication code from app_secret and data to sign.
+  """
   @spec hmac256_to_string(binary, binary) :: binary
   def hmac256_to_string(app_secret, to_sign) do
     :hmac.hmac256(app_secret, to_sign)
@@ -7,6 +10,9 @@ defmodule Poxa.CryptoHelper do
     |> String.from_char_list!
   end
 
+  @doc """
+  Compute a MD5, convert to hexadecimal and downcase it.
+  """
   @spec md5_to_string(binary) :: binary
   def md5_to_string(data) do
     data
