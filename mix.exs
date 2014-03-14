f9052eb96b5d1b769a10e84221b7017c02227115
Eduardo Gurgel <eduardo@gurgel.me>
2014-03-14 17:31:39 +1300

    Update to Elixir 0.12.4

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule Poxa.Mixfile do
     [ app: :poxa,
       version: "0.0.8",
       name: "Poxa",
-      elixir: "~> 0.12.2",
+      elixir: "~> 0.12.4",
       deps: deps(Mix.env),
       dialyzer: [ plt_apps: ["erts","kernel", "stdlib", "crypto", "public_key", "mnesia"],
                   flags: ["-Wunmatched_returns","-Werror_handling","-Wrace_conditions"]],
@@ -33,7 +33,7 @@ defmodule Poxa.Mixfile do
   defp deps(:dev) do
     [ {:cowboy, github: "extend/cowboy", tag: "0.9.0" },
       {:exlager, github: "khia/exlager"},
-      {:jsex, github: "talentdeficit/jsex", ref: "c9df36f07b2089a73ab6b32074c01728f1e5a2e1" },
+      {:jsex, github: "talentdeficit/jsex"},
       {:gproc, github: "uwiger/gproc", ref: "6e6cd7fab087edfaf7eb8a92a84d3c91cffe797c" },
       {:uuid, github: "avtobiff/erlang-uuid", tag: "v0.4.5" } ]
   end
