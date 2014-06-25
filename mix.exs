605b16e90a83ce09e0606351c0a2f0aa8a4dea30
Eduardo Gurgel <eduardo@gurgel.me>
2014-06-25 20:03:56 +1200

    Update to Elixir 0.14.0
    Also deps and travis config

--- mix.exs
+++ mix.exs
@@ -5,19 +5,16 @@ defmodule Poxa.Mixfile do
     [ app: :poxa,
       version: "0.1.1",
       name: "Poxa",
-      elixir: "~> 0.13.3",
-      deps: deps,
-      elixirc_options: options(Mix.env)]
+      elixir: "~> 0.14.0",
+      deps: deps ]
   end
 
   def application do
     [ applications: [ :exlager,
                       :crypto,
                       :gproc,
-                      :cowboy,
-                      :jsex,
-                      :uuid ],
-      included_applications: [ :signaturex ],
+                      :cowboy ],
+      included_applications: [ :jsex, :uuid, :signaturex ],
       mod: { Poxa, [] },
       env: [ port: 8080,
              app_key: "app_key",
@@ -30,22 +27,11 @@ defmodule Poxa.Mixfile do
     [ {:cowboy, github: "extend/cowboy", tag: "0.9.0" },
       {:exlager, github: "khia/exlager"},
       {:jsex, "~> 2.0"},
-      {:signaturex, "~> 0.0.3"},
+      {:signaturex, "~> 0.0.4"},
       {:gproc, github: "uwiger/gproc", ref: "6e6cd7fab087edfaf7eb8a92a84d3c91cffe797c" },
       {:uuid, github: "avtobiff/erlang-uuid", tag: "v0.4.5" },
       {:meck, github: "eproxus/meck", tag: "0.8.2", only: :test},
       {:pusher_client, github: "edgurgel/pusher_client", only: :test},
-      {:exrm, "0.6.4", only: :prod} ]
-  end
-
-  defp options(:dev) do
-    [exlager_level: :debug, exlager_truncation_size: 8096]
-  end
-
-  defp options(:test) do
-    [exlager_level: :critical, exlager_truncation_size: 8096]
-  end
-
-  defp options(_) do
+      {:exrm, "~> 0.9.0", only: :prod} ]
   end
 end
