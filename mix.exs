9a5d1516d454236082884deacd171853736a04b3
Eduardo Gurgel <eduardo@gurgel.me>
2014-08-10 14:38:26 +1200

    Update deps and use hex package where possible

--- mix.exs
+++ mix.exs
@@ -24,14 +24,14 @@ defmodule Poxa.Mixfile do
   end
 
   defp deps do
-    [ {:cowboy, github: "extend/cowboy", tag: "0.9.0" },
-      {:exlager, github: "khia/exlager"},
+    [ {:cowboy, "~> 1.0.0" },
       {:jsex, "~> 2.0"},
-      {:signaturex, "~> 0.0.4"},
+      {:signaturex, "~> 0.0.7"},
+      {:exlager, github: "khia/exlager"},
       {:gproc, github: "uwiger/gproc", ref: "6e6cd7fab087edfaf7eb8a92a84d3c91cffe797c" },
       {:uuid, github: "avtobiff/erlang-uuid", tag: "v0.4.5" },
       {:meck, github: "eproxus/meck", tag: "0.8.2", only: :test},
       {:pusher_client, github: "edgurgel/pusher_client", only: :test},
-      {:exrm, "~> 0.9.0", only: :prod} ]
+      {:exrm, "0.12.11", only: :prod} ]
   end
 end
