02d463395735e535a9a06f6607cabe75059b5499
Eduardo Gurgel <eduardo@gurgel.me>
2014-09-13 22:56:14 +1200

    Bump version 0.3.1
    [ci skip]

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Poxa.Mixfile do
 
   def project do
     [ app: :poxa,
-      version: "0.3.0",
+      version: "0.3.1",
       name: "Poxa",
       elixir: "~> 0.15.0 or ~> 1.0.0",
       deps: deps ]
