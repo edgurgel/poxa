aeaf7d61ee19e6ee3273c1fefe2e5323a195f132
Eduardo Gurgel <eduardo@gurgel.me>
2014-09-14 22:23:40 +1200

    Use isolated channel name on PresenceChannel test

--- test/presence_channel_test.exs
+++ test/presence_channel_test.exs
@@ -4,17 +4,17 @@ defmodule Poxa.PresenceChannelTest do
   import Poxa.PresenceChannel
 
   test "return unique user ids currently subscribed" do
-    register_to_channel("presence-channel", {:user_id, :user_info})
-    spawn_registered_process("presence-channel", {:user_id, :user_info})
-    spawn_registered_process("presence-channel", {:user_id2, :user_info2})
+    register_to_channel("presence-channel-test", {:user_id, :user_info})
+    spawn_registered_process("presence-channel-test", {:user_id, :user_info})
+    spawn_registered_process("presence-channel-test", {:user_id2, :user_info2})
 
-    assert users("presence-channel") == [:user_id, :user_id2]
+    assert users("presence-channel-test") == [:user_id, :user_id2]
   end
 
   test "return number of unique subscribed users" do
-    register_to_channel("presence-channel", {:user_id, :user_info})
-    spawn_registered_process("presence-channel", {:user_id2, :user_info2})
+    register_to_channel("presence-channel-test2", {:user_id, :user_info})
+    spawn_registered_process("presence-channel-test2", {:user_id2, :user_info2})
 
-    assert user_count("presence-channel") == 2
+    assert user_count("presence-channel-test2") == 2
   end
 end
