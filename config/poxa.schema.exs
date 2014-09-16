aaccda5d7b342ea8328d413dbd33e3bafb737c16
Eduardo Gurgel <eduardo@gurgel.me>
2014-09-16 22:27:20 +1200

    Add conform generated conf and schema

---
+++ config/poxa.schema.exs
@@ -0,0 +1,30 @@
+[
+  mappings: [
+    "poxa.port": [
+      doc: "HTTP port",
+      to: "poxa.port",
+      datatype: :integer,
+      default: 8080
+    ],
+    "poxa.app_key": [
+      doc: "Pusher app key",
+      to: "poxa.app_key",
+      datatype: :binary,
+      default: "app_key"
+    ],
+    "poxa.app_secret": [
+      doc: "Pusher secret",
+      to: "poxa.app_secret",
+      datatype: :binary,
+      default: "secret"
+    ],
+    "poxa.app_id": [
+      doc: "Pusher app id",
+      to: "poxa.app_id",
+      datatype: :binary,
+      default: "app_id"
+    ]
+  ],
+  translations: [
+  ]
+]
