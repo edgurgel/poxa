f5630de5694d2c7fba638c07d1b5da484dca1194
Eduardo Gurgel <eduardo@gurgel.me>
2015-05-19 21:14:04 +1200

    Remove unnecessary setup on SocketIdTest

--- test/socket_id_test.exs
+++ test/socket_id_test.exs
@@ -2,11 +2,6 @@ defmodule Poxa.SocketIdTest do
   use ExUnit.Case
   alias Poxa.SocketId
 
-  setup_all do
-    :random.seed
-    :ok
-  end
-
   test "generate! returns a string of 2 integers" do
     socket_id = SocketId.generate!
     [part1, part2] = String.split(socket_id, ".")
