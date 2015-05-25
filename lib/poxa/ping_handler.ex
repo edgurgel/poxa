40d95d7895318875df3029fa404e4fecbc8addd8
Eduardo Gurgel <eduardo@gurgel.me>
2015-05-25 20:44:34 +1200

    Document Poxa.PingHandler
    [ci skip]

--- lib/poxa/ping_handler.ex
+++ lib/poxa/ping_handler.ex
@@ -1,13 +1,19 @@
 defmodule Poxa.PingHandler do
+  @moduledoc """
+  Simple ping endpoint that returns 200 "Pong!" for any request
+  """
   require Logger
+
+  @doc false
   def init(_transport, req, _Opts), do: {:ok, req, nil}
 
+  @doc false
   def handle(req, state) do
     {:ok, req} = :cowboy_req.reply(200, [], "Pong!", req)
     Logger.info "Ping requested"
     {:ok, req, state}
   end
 
+  @doc false
   def terminate(_, _, _), do: :ok
-
 end
