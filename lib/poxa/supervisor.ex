9f0d079fc7df70c3dbef9c7f8330de78a7dc245a
Eduardo Gurgel <eduardo@gurgel.me>
2015-06-04 21:48:09 +1200

    Add missing docs to Poxa.Supervisor

--- lib/poxa/supervisor.ex
+++ lib/poxa/supervisor.ex
@@ -1,10 +1,14 @@
 defmodule Poxa.Supervisor do
   use Supervisor
 
+  @doc false
   def start_link do
     :supervisor.start_link({:local, __MODULE__}, __MODULE__, [])
   end
 
+  @doc """
+  The supervisor will spawn a GenEvent named Poxa.Event
+  """
   def init([]) do
     children = [worker(GenEvent, [[name: Poxa.Event]])]
     supervise children, strategy: :one_for_one
