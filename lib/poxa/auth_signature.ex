83e2ccc8b0109c6c6b6aaa6c2a9f8757d0c6ecae
Eduardo Gurgel <eduardo@gurgel.me>
2015-09-06 12:40:13 +1200

    Change Poxa.AuthSignature to support multiple apps

--- lib/poxa/auth_signature.ex
+++ lib/poxa/auth_signature.ex
@@ -4,25 +4,21 @@ defmodule Poxa.AuthSignature do
   http://pusher.com/docs/auth_signatures
   """
 
-  require Logger
-  alias Poxa.CryptoHelper
-
   @doc """
   Validate auth signature as described at: http://pusher.com/docs/auth_signatures .
   Returns true or false
   """
-  @spec valid?(binary, binary) :: boolean
-  def valid?(to_sign, auth) do
-    case String.split(auth, ":", parts: 2) do
-      [app_key, remote_signed_data] ->
-        signed_data = sign_data(to_sign)
-        Poxa.Authentication.check_key(app_key) and signed_data == remote_signed_data
-      _ -> false
+  @spec valid?(binary, binary, binary) :: boolean
+  def valid?(app_id, to_sign, auth) do
+    case Poxa.App.key_secret(app_id) do
+      {:ok, {app_key, app_secret}} ->
+        case String.split(auth, ":", parts: 2) do
+          [^app_key, remote_signed_data] ->
+            signed_data = Poxa.CryptoHelper.hmac256_to_string(app_secret, to_sign)
+            signed_data == remote_signed_data
+          _ -> false
+        end
+      {:error, _} -> false
     end
-  end
-
-  defp sign_data(data) do
-    {:ok, app_secret} = :application.get_env(:poxa, :app_secret)
-    CryptoHelper.hmac256_to_string(app_secret, data)
   end
 end
