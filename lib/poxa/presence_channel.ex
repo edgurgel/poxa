9d4521bc35dc552f63bf300e60e22362693a9afa
Eduardo Gurgel <eduardo@gurgel.me>
2015-09-06 13:20:30 +1200

    Change Poxa.PresenceChannel to support multiple apps

--- lib/poxa/presence_channel.ex
+++ lib/poxa/presence_channel.ex
@@ -8,9 +8,9 @@ defmodule Poxa.PresenceChannel do
 
   More info at: http://pusher.com/docs/rest_api#method-get-users
   """
-  @spec users(binary) :: [binary | integer]
-  def users(channel) do
-    match = {{:p, :l, {:pusher, channel}}, :_, :'$1'}
+  @spec users(binary, binary) :: [binary | integer]
+  def users(channel, app_id) do
+    match = {{:p, :l, {:pusher, app_id, channel}}, :_, :'$1'}
     :gproc.select([{match, [], [:'$1']}])
     |> Enum.uniq(fn {user_id, _} -> user_id end)
     |> Enum.map(fn {user_id, _} -> user_id end)
@@ -19,9 +19,9 @@ defmodule Poxa.PresenceChannel do
   @doc """
   Returns the number of unique users on a presence channel
   """
-  @spec user_count(binary) :: non_neg_integer
-  def user_count(channel) do
-    match = {{:p, :l, {:pusher, channel}}, :_, :'$1'}
+  @spec user_count(binary, binary) :: non_neg_integer
+  def user_count(channel, app_id) do
+    match = {{:p, :l, {:pusher, app_id, channel}}, :_, :'$1'}
     :gproc.select([{match, [], [:'$1']}])
     |> Enum.uniq(fn {user_id, _} -> user_id end)
     |> Enum.count
