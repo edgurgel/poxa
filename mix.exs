08450fb764890b64805fc5ae97b68d7ad4e24929
Eduardo Gurgel <eduardo@gurgel.me>
2016-03-03 22:17:04 +1300

    Update signaturex pusher and inch_ex packages

--- mix.exs
+++ mix.exs
@@ -17,14 +17,14 @@ defmodule Poxa.Mixfile do
   defp deps do
     [ {:cowboy, "~> 1.0.0" },
       {:exjsx, "~> 3.0"},
-      {:signaturex, "~> 0.0.8"},
+      {:signaturex, "~> 1.0"},
       {:gproc, "~> 0.3.0"},
       {:meck, "~> 0.8.2", only: :test},
       {:pusher_client, github: "edgurgel/pusher_client", only: :test},
-      {:pusher, "~> 0.1.0", only: :test},
+      {:pusher, "~> 0.1", only: :test},
       {:exrm, "~> 0.19.2", only: :prod},
       {:edip, "~> 0.4", only: :prod},
-      {:inch_ex, only: :docs},
+      {:inch_ex, "~> 0.5.1", only: :docs},
       {:httpoison, "~> 0.8"},
       {:watcher, "~> 1.0.0"} ]
   end
