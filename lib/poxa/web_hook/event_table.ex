9fa67f6b686a70c15cc81de4f1cc0422b929206c
Iuri Fernandes <iurifq@gmail.com>
2016-03-13 02:55:14 +0100

    Make EventTable.insert aware of opposite events

--- lib/poxa/web_hook/event_table.ex
+++ lib/poxa/web_hook/event_table.ex
@@ -19,14 +19,21 @@ defmodule Poxa.WebHook.EventTable do
   milliseconds corresponding to how much time each event must be delayed before
   being sent. This `delay` is summed up with the current timestamp and used
   as key of the ETS table.
+
+  It doesn't always insert a new value, though. In the case where there is a
+  corresponding event(e.g. `member_added` is being inserted when `member_removed`
+  of the same user and channel is already in the the table), it does not insert the
+  new event but it removes the old one. This is done to achieve delaying events as
+  it is described in [pusher's documentation](https://pusher.com/docs/webhooks#delay)
   """
   def insert(_, delay \\ 0)
   def insert(event, delay) when not is_list(event), do: insert([event], delay)
   def insert(events, delay) do
-    Enum.each events, fn(event) ->
-      true = :ets.insert(@table_name, {time_ms + delay, event})
+    Enum.filter events, fn(event) ->
+      if delete_corresponding(event) == 0 do
+        :ets.insert(@table_name, {time_ms + delay, event})
+      end
     end
-    true
   end
 
   @doc """
@@ -49,6 +56,24 @@ defmodule Poxa.WebHook.EventTable do
   before a certain timestamp.
   """
   def clear_older(timestamp), do: select_delete(@table_name, [{{:"$1", :"$2"}, [], [{:<, :"$1", timestamp}]}])
+
+  defp delete_corresponding(%{name: "channel_occupied", channel: channel}) do
+    match = [
+      {{:_, %{channel: :"$1", name: "channel_vacated"}},
+      [],
+      [{:==, {:const, channel}, :"$1"}]}
+    ]
+    select_delete(@table_name, match)
+  end
+  defp delete_corresponding(%{name: "member_added", channel: channel, user_id: user_id}) do
+    match = [
+      {{:_, %{channel: :"$1", name: "member_removed", user_id: :"$2"}},
+      [],
+      [{:andalso, {:==, {:const, user_id}, :"$2"}, {:==, {:const, channel}, :"$1"}}]}
+    ]
+    select_delete(@table_name, match)
+  end
+  defp delete_corresponding(_), do: 0
 
   defp time_ms, do: :erlang.system_time(:milli_seconds)
 end
