3a5cc8bb1299e60844952c2101f372ff2899c7e5
Eduardo Gurgel <eduardo@gurgel.me>
2019-12-14 18:54:50 +1300

    Bump version 1.1.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Poxa.Mixfile do
   def project do
     [
       app: :poxa,
-      version: "1.1.0",
+      version: "1.1.1",
       name: "Poxa",
       elixir: "~> 1.9",
       deps: deps(),
